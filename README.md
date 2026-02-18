62dc1edcec5c5c6486c4eaad956e2571d0034386
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-02-18 19:49:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nix flake for fluxer
+
+A nix flake that wraps the appImage in a nix flake so you can install it easily.
+
+Will probably be archieved once fluxer gets packaged for nixpkgs, also the hash needs manual updating so if I forget it, feel free to open a PR
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs.flakelight.url = "github:nix-community/flakelight";
+
+  outputs = {flakelight, ...}:
+    flakelight ./. {
+      package = {
+        appimageTools,
+        fetchurl,
+        makeDesktopItem,
+      }: let
+        pname = "fluxer";
+        version = "0.0.1";
+
+        src = fetchurl {
+          url = "https://api.fluxer.app/dl/desktop/stable/linux/x64/latest/appimage";
+          sha256 = "sha256-GdoBK+Z/d2quEIY8INM4IQy5tzzIBBM+3CgJXQn0qAw=";
+        };
+
+        desktopItem = makeDesktopItem {
+          name = pname;
+          exec = pname;
+          icon = pname;
+          desktopName = "Fluxer";
+          genericName = "Fluxer Desktop";
+          categories = ["Utility"];
+          terminal = false;
+          startupWMClass = "fluxer_app";
+        };
+      in
+        appimageTools.wrapType2 {
+          inherit pname version src;
+
+          extraInstallCommands = ''
+            mkdir -p $out/share/applications
+            cp ${desktopItem}/share/applications/* $out/share/applications/
+
+            install -m 444 -D ${./logo.png} $out/share/icons/hicolor/256x256/apps/${pname}.png
+          '';
+        };
+    };
+}
